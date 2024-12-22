e3313c7d88bff9f69da9c7729c5ffaaad68917e5
FarhanChowdhury <76951531+farhanchowdhury738@users.noreply.github.com>
2024-12-22 23:56:47 +0600

    Update Stack_Using_Array.cpp

--- Theory_FINAL/Stack/Stack_Using_Array.cpp
+++ Theory_FINAL/Stack/Stack_Using_Array.cpp
@@ -1,48 +1,108 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class myStack{
-    public: 
-        vector<int> v;
+#define MAXSIZE 5
 
-    void push(int val){
-        v.push_back(val);
-    }
-    void pop(){
-        v.pop_back();
-    }
-    int top(){
-        return v.back();
-    }
-    int size(){
-        return v.size();
-    }
-    bool empty(){
-        if(v.size()==0) return true;
-        else return false;
-    }
-};
+int s[MAXSIZE]; // Array to store the stack elements
+int top;        // Index for the top element
 
-int main()
-{
-    cout<<"Helloworld"<<endl;
+// Function prototypes
+void push(int value);
+int pop();
+int topp();
+int isStackEmpty();
+int isStackFull();
+void display_stack();
+int stackSize();
 
-    myStack st;
-    st.push(10);
-    st.push(20);
-    st.push(30);
+// Function implementations
+void stack_initialize() {
+    top = -1;
+}
 
-    // cout<<st.top()<<endl;
-    // st.pop();
-    // cout<<st.top()<<endl;
-    // st.pop();
-    // cout<<st.top()<<endl;
-
-    while(!st.empty()){
-        cout<<st.top()<<endl;
-        st.pop();
+void push(int value) {
+    if (isStackFull()) {
+        cout << "Stack is full";
+    } else {
+        top++;
+        s[top] = value;
     }
+}
+
+int pop() {
+    if (isStackEmpty()) {
+        cout << "Underflow: Stack is empty";
+        return 0;
+    } else {
+        int x = s[top];
+        top--;
+        return x;
+    }
+}
+
+int isStackEmpty() {
+    return (top == -1);
+}
+
+int isStackFull() {
+    return (top >= MAXSIZE - 1);
+}
+
+int stackSize() {
+    return top + 1;
+}
+
+void display_stack() {
+    for (int i = top; i >= 0; i--) {
+        cout << " " << s[i];
+    }
+}
+
+int topp() {
+    return s[top];
+}
+
+int main() {
+    int option, n, value;
+    stack_initialize();
+
+    do {
+        cout << "\n\n 1. Push an element in stack";
+        cout << "\n 2. Pop an element from stack";
+        cout << "\n 3. Display stack";
+        cout << "\n 4. Display current stack size";
+        cout << "\n 5. Display top element";
+        cout << "\n 6. Exit";
+        cout << "\n Enter your choice: ";
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                cout << "\n Insert an element in stack: ";
+                cin >> n;
+                push(n);
+                break;
+            case 2:
+                value = pop();
+                if (value != 0)
+                    cout << "\n Popped element is: " << value << endl;
+                break;
+            case 3:
+                display_stack();
+                break;
+            case 4:
+                cout << "Stack size: " << stackSize();
+                break;
+            case 5:
+                cout << "Top element: " << topp() << endl;
+                break;
+            case 6:
+                return 0;
+            default:
+                cout << "Invalid choice. Please try again.";
+        }
+
+    } while (option != 6);
 
     return 0;
 }
